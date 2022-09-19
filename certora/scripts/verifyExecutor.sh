2e3a71380a6f21d7733ce84b32236980bebd45ac
Michael M <91594326+MichaelMorami@users.noreply.github.com>
2022-09-20 00:45:07 +0300

    certora security review

---
+++ certora/scripts/verifyExecutor.sh
@@ -0,0 +1,7 @@
+certoraRun src/contracts/Executor.sol:Executor \
+    --verify Executor:certora/specs/executor.spec \
+    --solc solc8.8 \
+    --optimistic_loop \
+    --cloud \
+    --msg "Executor:executor.spec $1"
+    
