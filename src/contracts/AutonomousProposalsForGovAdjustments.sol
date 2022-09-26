37038ce45e9f08ab743a5895612e6853e61c056b
eboado <ebdmrr@gmail.com>
2022-09-26 19:41:18 +0200

    Added usage of solidity-utils

--- src/contracts/AutonomousProposalsForGovAdjustments.sol
+++ src/contracts/AutonomousProposalsForGovAdjustments.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.0;
 
 import {AaveGovernanceV2, IExecutorWithTimelock, IGovernanceStrategy} from 'aave-address-book/AaveGovernanceV2.sol';
-import {IERC20} from 'openzeppelin-contracts/token/ERC20/IERC20.sol';
-import {SafeERC20} from 'openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol';
+import {IERC20} from 'solidity-utils/contracts/oz-common/interfaces/IERC20.sol';
+import {SafeERC20} from 'solidity-utils/contracts/oz-common/SafeERC20.sol';
 
 /**
  * @title AutonomousProposalsForGovAdjustments
